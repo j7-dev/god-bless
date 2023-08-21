932f57932e3293ef34f2bd8388c9a2a810ce5e18
TW-IT-Jerry Liu <jerry.liu@shoalter.com>
2023-08-21 17:39:02 +0800

    feat: change folder name

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 /* eslint-disable @typescript-eslint/no-shadow */
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const rootDir = path.resolve(__dirname, '..');
+const rootDir = path.resolve(__dirname, "..");
 const buildDir = `${rootDir}/build`;
 
 // 讀取bless.txt的內容
-fs.readFile(`${rootDir}/bless/bless.txt`, 'utf8', (err, data) => {
+fs.readFile(`${rootDir}/god-bless/bless.txt`, "utf8", (err, data) => {
+  if (err) {
+    console.error("❌ 讀取檔案時發生錯誤：", err);
+    return;
+  }
+
+  // 讀取index.html的內容
+  fs.readFile(`${buildDir}/index.html`, "utf8", (err, htmlData) => {
     if (err) {
-        console.error('❌ 讀取檔案時發生錯誤：', err);
-        return;
+      console.error("❌ 讀取檔案時發生錯誤：", err);
+      return;
     }
 
-    // 讀取index.html的內容
-    fs.readFile(`${buildDir}/index.html`, 'utf8', (err, htmlData) => {
-        if (err) {
-            console.error('❌ 讀取檔案時發生錯誤：', err);
-            return;
-        }
+    // 在index.html的最上方插入bless.txt的內容
+    const updatedHtml = `${data}\n${htmlData}`;
 
-        // 在index.html的最上方插入bless.txt的內容
-        const updatedHtml = `${data}\n${htmlData}`;
-
-        // 寫回更新後的內容到index.html
-        fs.writeFile(`${buildDir}/index.html`, updatedHtml, 'utf8', (err) => {
-            if (err) {
-                console.error('❌ 寫入檔案時發生錯誤：', err);
-            } else {
-                console.log('✅ 成功插入內容到index.html');
-            }
-        });
+    // 寫回更新後的內容到index.html
+    fs.writeFile(`${buildDir}/index.html`, updatedHtml, "utf8", (err) => {
+      if (err) {
+        console.error("❌ 寫入檔案時發生錯誤：", err);
+      } else {
+        console.log("✅ 成功插入內容到index.html");
+      }
     });
+  });
 });
